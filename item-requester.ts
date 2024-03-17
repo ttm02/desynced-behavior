35a744d351aad6a87ffa02141672a9c5fc1f318e
Tim Jammer <tim.jammer@tu-darmstadt.de>
2024-03-17 16:24:15 +0100

    fix item requester and added stroage out

--- item-requester.ts
+++ item-requester.ts
@@ -1,7 +1,16 @@
 export function request_ingredients(produced_item: Value) {
+    
+    // Wait until recipe is set
+    	while (compareItem(produced_item, null)) {
+        // try to read item from surrounding buildings
+        // e.g. the factroy block manager, as no other entitiy in range will set their signal register
+        for (const building of entitiesInRange(1, "v_building")) { 
+            produced_item = readSignal(building)
+        }
+    }
 
     let num_ingredients: number;
-    num_ingredients = 1;//or 2 to leave more space for the produced item?
+    num_ingredients = 0;
     for (const ingredient of recipieIngredients(produced_item)) { num_ingredients = num_ingredients + 1; }
 
 
@@ -17,7 +26,7 @@ export function request_ingredients(produced_item: Value) {
     // setup: lock the slots
     let current_slot: number = 1;
     for (const ingredient of recipieIngredients(produced_item)) {
-        for (let i = 1; 1 <= slots_per_ingredient; i = i + 1) {
+        for (let i = 1; i <= slots_per_ingredient; i = i + 1) {
             lockSlots(ingredient, current_slot)
             current_slot = current_slot + 1;
         }
---
+++ reserve_items.ts
@@ -0,0 +1,12 @@
+export function reserve_storage(item: Value) {
+
+    // Wait until item is set
+    while (compareItem(item, null)) {
+        // try to read item from surrounding buildings
+        // e.g. the factroy block manager, as no other entitiy in range will set their signal register
+        for (const building of entitiesInRange(1, "v_building")) { 
+            item = readSignal(building)
+        }
+    }
+    lockSlots(item, null)
+}
