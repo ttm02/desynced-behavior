b36f1b0eca457e22bab2c5cd8b498e0040cdbb64
Tim Jammer <tim.jammer@tu-darmstadt.de>
2024-03-17 20:45:15 +0100

    fix factory block worker

--- factroy_block_worker.ts
+++ factroy_block_worker.ts
@@ -3,19 +3,26 @@ export function factory_block_worker(produced_item: Value) {
     // Wait until recipe is set
     let manager_entity: Value;// the factory block manager (aka the building nearby that emits a signal)
 
+
     while (compareItem(produced_item, null)) {
         // try to read item from surrounding buildings
         // e.g. the factroy block manager, as no other entitiy in range will set their signal register
+            let local_produced_item: Value;
         for (const building of entitiesInRange(1, "v_building")) {
-            produced_item = readSignal(building)
-            if (!compareItem(produced_item, null)) {
+            local_produced_item = readSignal(building)
+            if (!compareItem(local_produced_item, null)) {
                 manager_entity = building;
+                if (!compareItem(produced_item, null)){
+                notify("ERROR: Multiple requested items?");}
+                produced_item=local_produced_item;
             }
         }
     }
 
     // get production component from network    
-    let component: Value = get_producing_component(produced_item)
+    let component: Value;
+    get_producing_component(produced_item,component);
+
     connect();
     requestItem(combineRegister(2, component));
     while (self.count(component) < 2) {
@@ -50,41 +57,43 @@ export function factory_block_worker(produced_item: Value) {
     // lock relevant slots    
     let current_slot = 1;
     for (const ingredient of recipieIngredients(produced_item)) {
-        for (var i; i <= slots_per_ingredient; i = i + 1) {
-            lockSlots(ingredient, 1);
+        for (var i=1; i <= slots_per_ingredient; i = i + 1) {
+            lockSlots(ingredient, current_slot);
             current_slot = current_slot + 1
         }
     }
     // for the result
-    for (var i; i <= slots_per_ingredient; i = i + 1) {
-        lockSlots(produced_item, 1);
+    for (var i=1; i <= slots_per_ingredient; i = i + 1) {
+        lockSlots(produced_item, current_slot);
         current_slot = current_slot + 1
     }
 
-    // the rest is handled by the factory block manager
+    // the rest (aka logistics) is handled by the factory block manager
+    exit();
 }
 
-function get_producing_component(produced_item: Value) {
 
-    let component: Value = "c_robotics_factory";
+function get_producing_component(produced_item: Value,component:Value) {
+
+    component = value("c_robotics_factory",1);
     if (canProduce(produced_item, component)) {
-        return component;
-    }
-    /*
-    component: Item = combineRegister(1,"c_assembler");
+        return;
+    }       
+    component = value("c_assembler",1);
     if (canProduce(produced_item, component)) {
-        return component;
+        return;
     }
-    component: Item = combineRegister(1,"c_fabricator");
+
+    component = value("c_fabricator",1);
     if (canProduce(produced_item, component)) {
-        return component;
+        return;
     }
-    component: Item = combineRegister(1,"c_refinery");
+    component = value("c_refinery",1);
     if (canProduce(produced_item, component)) {
-        return component;
+    return;
     }
-*/
-    notify("ERROR: Dont know where to produce item");
+
+    notify("ERROR: Dont know where to produce item");    
     return;
 
 }
